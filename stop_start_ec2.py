ac331ae76b31b1dc075f5905b486e608e326e17b
hseera <harinderseera@gmail.com>
2021-08-13 12:44:06 +1000

    Update stop_start_ec2.py

--- stop_start_ec2.py
+++ stop_start_ec2.py
@@ -21,7 +21,6 @@ import boto3
 from botocore.config import Config
 from pandas import DataFrame
 import sys
-import pandas as pd
 
 REGION_CONFIG = Config(
     region_name = 'ap-southeast-2',
@@ -75,7 +74,7 @@ def instanceid(option):
                     )
             print(response)
         else:
-            print("unrecognized option")
+            print("Unrecognized option")
     except Exception as e:
         print(e)
 
@@ -102,7 +101,7 @@ def instance_type(option, instancetype):
                     )
             print(response)
         else:
-            print("unrecognized option")
+            print("Unrecognized option")
     except Exception as e:
         print(e)
 
@@ -130,26 +129,55 @@ def platform(option, platform):
                     )
             print(response)
         else:
-            print("unrecognized option")
+            print("Unrecognized option")
+    except Exception as e:
+        print(e)
+
+def platform_instancetype(option, platform, instancetype):
+    try:
+        ec2_info = describe_ec2()
+        df = DataFrame(ec2_info,columns=['InstanceId','InstanceType','State-Code','State-Name','Platform'])
+        if option =="stop":
+            get_ec2_list = df.loc[(df['Platform'] == platform) & (df['State-Name']=='running')  & (df['InstanceType']==instancetype)]
+            instance_id = get_ec2_list['InstanceId'].values.tolist()
+            response = CLIENT.stop_instances(
+                    InstanceIds= instance_id,
+                    DryRun=False,
+                    Force=False
+                    )
+            print(response)
+        elif option =="start":
+            get_ec2_list = df.loc[(df['Platform'] == platform) & (df['State-Name']=='stopped')  & (df['InstanceType']==instancetype)]
+            instance_id = get_ec2_list['InstanceId'].values.tolist()
+            response = CLIENT.start_instances(
+                    InstanceIds= instance_id,
+                    DryRun=False
+                    )
+            print(response)
+        else:
+            print("Unrecognized option")
     except Exception as e:
         print(e)
     
-    
 def main(argv):
     if len(sys.argv) == 1:
-        print("Please pass in the argument and their values")
+        print("Please pass in the correct script arguments")
     elif len(sys.argv) == 2:
-        print("Please pass in the argument and their values")
+        print("Please pass in the correct script arguments")
     elif len(sys.argv) == 3:
         if str(sys.argv[2])=="id":
             instanceid(str(sys.argv[1]))
     elif len(sys.argv) == 4:
         if str(sys.argv[2]) == "type":
             instance_type(str(sys.argv[1]),str(sys.argv[3]))
-        if str(sys.argv[2]) == "platform":
+        elif str(sys.argv[2]) == "platform":
             platform(str(sys.argv[1]),str(sys.argv[3]))
+        elif str(sys.argv[2]) == "windows" or str(sys.argv[2]) =="other":
+            platform_instancetype(str(sys.argv[1]),str(sys.argv[2]), str(sys.argv[3]))
+        else:
+            print("Unrecognized arguments")
     else:
-        print("wrong number of parameters")
+        print("Wrong number of arguments or unrecognized arguments")
 
 
 
